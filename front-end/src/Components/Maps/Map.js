3605cf776ad6a9a2eb0224a2428bca20c0aa28b3
Aryan <aryanovalekar@gmail.com>
2021-09-04 19:41:10 -0400

    Add Map.js

--- front-end/src/Components/Maps/Map.js
+++ front-end/src/Components/Maps/Map.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {render} from 'react-dom';
+import MapGL, {GeolocateControl} from 'react-map-gl';
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiYXJ5YW5vdmFsZWthciIsImEiOiJja3Q2Y2w0bTYwaDJwMm5vMHh0NHJydnUwIn0.jBc3OwM4kr5hJCRv9ubQgQ'; // Set your mapbox token here
+
+const geolocateStyle = {
+  top: 0,
+  left: 0,
+  margin: 10
+};
+
+const positionOptions = {enableHighAccuracy: true};
 
 const Map = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [viewport, setViewport] = useState({
+    latitude: 37.8,
+    longitude: 96,
+    zoom: 3,
+    bearing: 0,
+    pitch: 0,
+    width:"100vw",
+    height:"100vh"
+  });
+
+  return (
+    <MapGL
+      {...viewport}
+      
+      mapboxApiAccessToken={MAPBOX_TOKEN}
+      onViewportChange={setViewport}
+    >
+    <GeolocateControl
+        style={geolocateStyle}
+        positionOptions={positionOptions}
+        trackUserLocation
+        auto
+      />
+    </MapGL>
+  )
 }
 
 export default Map
