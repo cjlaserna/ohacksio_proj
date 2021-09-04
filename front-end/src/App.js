b7144e5497801d3b28b4ec111047a998c66ab2cf
Aryan <aryanovalekar@gmail.com>
2021-09-04 16:17:23 -0400

    Add Routing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,30 @@
+import { BrowserRouter as Router, Route, useParams } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello World</p>
-    </div>
+    <Router>
+  
+    <Route exact path="/">
+      <h1>Dashboard</h1>
+    </Route>
+
+    <Route exact path="/login">
+      <h1>Login</h1>
+    </Route>
+
+    <Route exact path="/registration">
+      <h1>Registration</h1>
+    </Route>
+
+    <Route exact path="/login-failed">
+      <h1>Login Failed</h1>
+    </Route>
+
+    <Route exact path="/registration-failed">
+      <h1>Registration Failed</h1>
+    </Route>
+  
+  </Router>
   );
 }
 
