6ef71b8ddb423d7961c9fe05ba49249b9538ec4d
Aryan <aryanovalekar@gmail.com>
2021-09-04 20:21:38 -0400

    Update Map.js

--- front-end/src/Components/Maps/Map.js
+++ front-end/src/Components/Maps/Map.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState, useCallback} from 'react';
 import {render} from 'react-dom';
-import MapGL, {GeolocateControl, NavigationControl, FullscreenControl, ScaleControl} from 'react-map-gl';
+import MapGL, {GeolocateControl, NavigationControl, FullscreenControl, ScaleControl, Source} from 'react-map-gl';
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiYXJ5YW5vdmFsZWthciIsImEiOiJja3Q2Y2w0bTYwaDJwMm5vMHh0NHJydnUwIn0.jBc3OwM4kr5hJCRv9ubQgQ'; // Set your mapbox token here
 
@@ -37,21 +37,34 @@ const Map = () => {
     zoom: 3,
     bearing: 0,
     pitch: 0,
-    width:"100vw",
+    width:"100%",
     height:"100vh"
   });
+
+  const onMapLoad = useCallback(evt => {
+    const map = evt.target;
+    map.setTerrain({source: 'mapbox-dem', exaggeration: 1.5});
+  }, []);
 
   return (
     <MapGL
       {...viewport}
-      
       mapboxApiAccessToken={MAPBOX_TOKEN}
+      mapStyle="mapbox://styles/aryanovalekar/ckt6gmoy32zse17upfl1n30h7"
       onViewportChange={setViewport}
+      onLoad={onMapLoad}
     >
-    <GeolocateControl style={geolocateStyle} positionOptions={positionOptions} trackUserLocation auto/>
+    <GeolocateControl style={geolocateStyle} positionOptions={positionOptions} trackUserLocation auto showAccuracyCircle={false}/>
     <FullscreenControl style={fullscreenControlStyle} />
     <NavigationControl style={navStyle} />
     <ScaleControl style={scaleControlStyle} />
+    <Source
+          id="mapbox-dem"
+          type="raster-dem"
+          url="mapbox://mapbox.mapbox-terrain-dem-v1"
+          tileSize={512}
+          maxzoom={14}
+        />
     </MapGL>
   )
 }
