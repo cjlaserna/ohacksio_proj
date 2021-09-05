2daed66ee7ae78c7743b4f8004fc9c29d6eb74f3
Aryan <aryanovalekar@gmail.com>
2021-09-04 21:44:22 -0400

    Update ErrandCreate.js

--- front-end/src/Components/ErrandCreationPage/ErrandCreate.js
+++ front-end/src/Components/ErrandCreationPage/ErrandCreate.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ErrandCreate = () => {
     
@@ -7,11 +7,18 @@ const ErrandCreate = () => {
         console.log("submit")
     }
     
+    const [name, setName] = useState("");
+
     return (
         <div>
             <h1>Add an Errand</h1>
             <form onSubmit={formSubmit}>
+                <div>
+                    <label>Name: </label>
+                    <input type='input' placeholder = 'Errand Name' value={name} onChange={(e) => setName(e.target.value)}/>
+                </div>
 
+                <input type = 'submit' value =  'Create Errand'/>
             </form>
         </div>
     )
